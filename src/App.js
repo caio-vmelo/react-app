0b118cce6e1c905e6f4fa3b594e81891e97035d0
Caio Vinicius <106553681+caio-vmelo@users.noreply.github.com>
2023-05-03 14:49:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Mudanças aula 26-04-2023
+          DevOps Básico IT Talent - My Own Changes
+(conluido em 02.05)
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps - CI/CD
+          Learn DevOps
         </a>
       </header>
     </div>
